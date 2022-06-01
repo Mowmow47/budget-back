c386b59453f446565cb372eefd1ad5de7018cd56
Mowmow47 <morgan.duprat@gmail.com>
2022-06-01 12:16:40 +0200

    Implementation of the read and add routes in the account controller

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,24 +2,74 @@
 
 namespace App\Controller;
 
+use App\Entity\Account;
+use App\Form\AccountType;
+use App\Repository\AccountRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Controller used for (bank) account management.
+ * It currently retrieves all the account in the database as the api is still in its early stages of development.
+ */
+#[Route('/accounts', name: 'api_accounts_')]
 class AccountController extends AbstractController
 {
-    #[Route('/account', name: 'app_account')]
-    public function index(): JsonResponse
+    /**
+     * Get Accounts
+     * GET /accounts
+     * Returns a list of account (all the accounts stored in the database for now).
+     */
+    #[Route('', name: 'browse', methods: ['GET'])]
+    public function browse(AccountRepository $accountRepository): JsonResponse
     {
-        return $this->json([
-            '0' => [
-                'title' => 'Test 1',
-                'content' => 'Je suis un premier contenu de test',
-            ],
-            '1' => [
-                'title' => 'Test 2',
-                'content' => 'Je suis un second contenu de test',
-            ],
-        ]);
+        $accounts = $accountRepository->findAll();
+
+        return $this->json($accounts, Response::HTTP_OK);
+    }
+
+    /**
+     * Get Account
+     * GET /accounts/{id}
+     * Return the Account object for the fiven id. It uses parameter conversion to find the account associated with the specified id.
+     */
+    #[Route('/{id}',name: 'read', methods: ['GET'])]
+    public function read(Account $account): JsonResponse
+    {
+        return $this->json($account, Response::HTTP_OK);
+    }
+
+    /**
+     * Add Account
+     * POST /accounts
+     * add a new Account object in the database.
+     */
+    #[Route('', name: 'add', methods: ['POST'])]
+    public function add(AccountRepository $accountRepository, Request $request): JsonResponse
+    {
+        $account = new Account();
+
+        $form = $this->createForm(AccountType::class, $account);
+
+        $json = $request->getContent();
+        $jsonArray = json_decode($json, true);
+
+        $form->submit($jsonArray);
+
+        if($form->isValid()) {
+
+            /** I call the add method of the repository, natively present since symfony 5.4/6.0 from memory,
+             * instead of calling doctrine and persisting and flushing the entity directly here. */
+            $accountRepository->add($account, true);
+            
+            return $this->json($account, Response::HTTP_CREATED);
+        }
+
+        $errorsString = (string) $form->getErrors(true);
+
+        return $this->json($errorsString, Response::HTTP_BAD_REQUEST);
     }
 }
