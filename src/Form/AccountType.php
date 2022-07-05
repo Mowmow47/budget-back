f1eda70d20a87e2a928cb6577e25d8a4a444409d
Mowmow47 <morgan.duprat@gmail.com>
2022-07-05 14:51:42 +0200

    Corrections to the add/edit/delete methods of the account controller, first basic iteration of the add/edit accounts forms

--- src/Form/AccountType.php
+++ src/Form/AccountType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Account;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +13,9 @@ class AccountType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('accountNumber')
-            ->add('bankName')
-            ->add('interestRate')
+            ->add('name', TextType::class)
+            ->add('accountNumber', TextType::class)
+            ->add('bankName', TextType::class)
         ;
     }
 
